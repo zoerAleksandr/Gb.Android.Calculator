b112befc81f5b37481f8414beec53ec127074bab
Aleksandr <zoer.aleksandr@gmail.com>
2021-06-03 19:13:02 +0300

    homework #3. made a layout for the calculator app

---
+++ app/src/main/java/com/example/gbandroidcalculator/Calculator.java
@@ -0,0 +1,107 @@
+package com.example.gbandroidcalculator;
+
+import java.util.ArrayList;
+
+/**
+ * Project Gb.Android.Calculator
+ *
+ * @Author Zoer Aleksandr
+ * Created 31.05.2021 1:11
+ */
+
+// Что необходимо доделать
+// 1. Класс дженерик для сохранения результата ввода, тогда можно убрать 2/3 вычислительных методов
+// 2. Добавить еще один класс и вынести в него условия расчета
+public class Calculator {
+
+
+    ArrayList<String> digitList = new ArrayList<>();
+
+    protected void addDigit(String s) {
+        digitList.add(s);
+    }
+
+    private void clearList() {
+        digitList.clear();
+    }
+
+    protected String result() {
+        Integer a = null;
+        Integer b = null;
+
+        if (!Action.aDot) {
+            a = Integer.parseInt(digitList.get(0));
+            b = Integer.parseInt(digitList.get(1));
+            System.out.println("a = " + a + " b = " + b);
+        }
+
+        clearList();
+
+        if (Action.summation) {
+            return plus(a, b).toString();
+        } else if (Action.subtraction) {
+            return minus(a, b).toString();
+        } else if (Action.multiplication) {
+            return multiple(a, b).toString();
+        } else if (Action.division) {
+            return div(a, b).toString();
+        } else
+            return "0";
+    }
+
+
+    protected Integer plus(Integer a, Integer b) {
+        if (a != null && b != null) {
+            return a + b;
+        }
+        return null;
+    }
+
+    protected Double plus(Integer a, Double b) {
+        return a + b;
+    }
+
+    protected Double plus(Double a, Double b) {
+        return a + b;
+    }
+
+    protected Integer minus(Integer a, Integer b) {
+        System.out.println("result = " + (a - b));
+        return a - b;
+    }
+
+    protected Double minus(Double a, Integer b) {
+        return a - b;
+    }
+
+    protected Double minus(Integer a, Double b) {
+        return a - b;
+    }
+
+    protected Integer multiple(Integer a, Integer b) {
+        return a * b;
+    }
+
+    protected Double multiple(Double a, Integer b) {
+        return a * b;
+    }
+
+    protected Double multiple(Integer a, Double b) {
+        return a * b;
+    }
+
+    protected Integer div(Integer a, Integer b) {
+        if (b == 0) return null;
+        return a / b;
+    }
+
+    protected Double div(Double a, Integer b) {
+        if (b == 0) return null;
+        return a / b;
+    }
+
+    protected Double div(Integer a, Double b) {
+        if (b == 0) return null;
+        return a / b;
+    }
+}
--- app/src/main/java/com/example/gbandroidcalculator/MainActivity.java
+++ app/src/main/java/com/example/gbandroidcalculator/MainActivity.java
@@ -3,12 +3,154 @@ package com.example.gbandroidcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    StringBuilder sB = new StringBuilder();
+    TextView tv_result, tv_first_digit;
+    Calculator calc = new Calculator();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+        tv_result = findViewById(R.id.text_result);
+        tv_first_digit = findViewById(R.id.text_first_digit);
+
+        findViewById(R.id.btn_0).setOnClickListener(clickListener);
+        findViewById(R.id.btn_1).setOnClickListener(clickListener);
+        findViewById(R.id.btn_2).setOnClickListener(clickListener);
+        findViewById(R.id.btn_3).setOnClickListener(clickListener);
+        findViewById(R.id.btn_4).setOnClickListener(clickListener);
+        findViewById(R.id.btn_5).setOnClickListener(clickListener);
+        findViewById(R.id.btn_6).setOnClickListener(clickListener);
+        findViewById(R.id.btn_7).setOnClickListener(clickListener);
+        findViewById(R.id.btn_8).setOnClickListener(clickListener);
+        findViewById(R.id.btn_9).setOnClickListener(clickListener);
+        findViewById(R.id.btn_dot).setOnClickListener(clickListener);
+
+        findViewById(R.id.btn_result).setOnClickListener(clickActions);
+        findViewById(R.id.btn_plus).setOnClickListener(clickActions);
+        findViewById(R.id.btn_minus).setOnClickListener(clickActions);
+        findViewById(R.id.btn_multiple).setOnClickListener(clickActions);
+        findViewById(R.id.btn_div).setOnClickListener(clickActions);
+
     }
+
+    final View.OnClickListener clickActions = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+            if (v.getId() == R.id.btn_plus) {
+                if (Action.summation) {
+                    return;
+                } else if (Action.subtraction || Action.multiplication || Action.division) {
+                    Action.summation = true;
+                    Action.subtraction = false;
+                    Action.multiplication = false;
+                    Action.division = false;
+                    tv_first_digit.setText(sB.replace(sB.length() - 1, sB.length(), "+"));
+                } else {
+                    Action.summation = true;
+                    Action.subtraction = false;
+                    Action.multiplication = false;
+                    Action.division = false;
+                    calc.addDigit(sB.toString());
+                    tv_first_digit.setText(sB.append('+'));
+                }
+            } else if (v.getId() == R.id.btn_minus) {
+                if (Action.subtraction) {
+                    return;
+                } else if (Action.summation || Action.multiplication || Action.division) {
+                    Action.summation = false;
+                    Action.subtraction = true;
+                    Action.multiplication = false;
+                    Action.division = false;
+                    tv_first_digit.setText(sB.replace(sB.length() - 1, sB.length(), "-"));
+                } else {
+                    Action.summation = false;
+                    Action.subtraction = true;
+                    Action.multiplication = false;
+                    Action.division = false;
+                    calc.addDigit(sB.toString());
+                    tv_first_digit.setText(sB.append('-'));
+                }
+            } else if (v.getId() == R.id.btn_multiple) {
+                if (Action.multiplication) {
+                    return;
+                } else if (Action.summation || Action.subtraction || Action.division) {
+                    Action.summation = false;
+                    Action.subtraction = false;
+                    Action.multiplication = true;
+                    Action.division = false;
+                    tv_first_digit.setText(sB.replace(sB.length() - 1, sB.length(), "*"));
+                } else {
+                    Action.summation = false;
+                    Action.subtraction = false;
+                    Action.multiplication = true;
+                    Action.division = false;
+                    calc.addDigit(sB.toString());
+                    tv_first_digit.setText(sB.append('*'));
+                }
+            } else if (v.getId() == R.id.btn_div) {
+                if (Action.division) {
+                    return;
+                } else if (Action.summation || Action.subtraction || Action.multiplication) {
+                    Action.summation = false;
+                    Action.subtraction = false;
+                    Action.multiplication = false;
+                    Action.division = true;
+                    tv_first_digit.setText(sB.replace(sB.length() - 1, sB.length(), "/"));
+                } else {
+                    Action.summation = false;
+                    Action.subtraction = false;
+                    Action.multiplication = false;
+                    Action.division = true;
+                    calc.addDigit(sB.toString());
+                    tv_first_digit.setText(sB.append('/'));
+                }
+            } else if (v.getId() == R.id.btn_result) {
+                calc.addDigit(sB.substring(sB.indexOf(Action.getSymbol()) + 1));
+                String res = calc.result();
+                tv_result.setText(res);
+                tv_first_digit.setText("");
+                sB.replace(0, sB.length(), res);
+                calc.addDigit(sB.toString());
+            }
+        }
+    };
+
+    final View.OnClickListener clickListener = new View.OnClickListener() {
+
+        @Override
+        public void onClick(View v) {
+
+            if (v.getId() == R.id.btn_0) {
+                sB.append("0");
+            } else if (v.getId() == R.id.btn_1) {
+                sB.append("1");
+            } else if (v.getId() == R.id.btn_2) {
+                sB.append("2");
+            } else if (v.getId() == R.id.btn_3) {
+                sB.append("3");
+            } else if (v.getId() == R.id.btn_4) {
+                sB.append("4");
+            } else if (v.getId() == R.id.btn_5) {
+                sB.append("5");
+            } else if (v.getId() == R.id.btn_6) {
+                sB.append("6");
+            } else if (v.getId() == R.id.btn_7) {
+                sB.append("7");
+            } else if (v.getId() == R.id.btn_8) {
+                sB.append("8");
+            } else if (v.getId() == R.id.btn_9) {
+                sB.append("9");
+            }
+            tv_result.setText(sB.toString());
+        }
+    };
 }
